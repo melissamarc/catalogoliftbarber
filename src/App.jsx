18c6f43a9f6e4b7531b284a2e3485ab60b1caee9
Melissa Marcielo <melissamarcielo@192.168.1.102>
2026-06-17 17:05:49 -0300

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Catalogo from "./pages/Catalogo";
 import Admin from "./pages/Admin";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <BrowserRouter>
+    <Analytics/>
       <Routes>
         <Route path="/" element={<Catalogo />} />
         <Route path="/admin" element={<Admin />} />
